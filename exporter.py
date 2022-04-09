c9d6bbe7ae2b6816450018016a6229f904e210ad
rosenrose <crazytempler@gmail.com>
2022-04-09 15:11:02 +0900

    challenge

--- exporter.py
+++ exporter.py
@@ -3,8 +3,9 @@ import csv
 def save_to_file(filename, jobs):
     with open(filename, "w", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(jobs[0].keys())
+        writer.writerow(list(jobs.values())[0][0].keys())
 
-        for job in jobs:
-            writer.writerow(job.values())
+        for site in jobs:
+            for job in jobs[site]:
+                writer.writerow(job.values())
     return csv
---
+++ stackoverflow.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_last_page(url):
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    try:
+        pagination = soup.select_one("div.s-pagination")
+        links = pagination.select("a")
+        # print(links)
+        pages = [int(link.text) for link in links[:-1]]
+        max_page = pages[-1]
+    except:
+        return 1
+
+    return max_page
+
+def extract_job(html):
+    try:
+        title = html.select_one("a[title]")["title"]
+        company = html.select_one("h3 span:not([class])").text.strip()
+        job_id = html["data-jobid"]
+    except Exception as e:
+        print(e, html)
+        return {}
+
+    try:
+        location = html.select_one("h3 span[class]").text.strip()
+    except:
+        location = ""
+
+    return {
+        "site": "Stackoverflow",
+        "title": title,
+        "company": company,
+        "location": location,
+        "link": f"https://stackoverflow.com/jobs/{job_id}/"
+    }
+
+def extract_jobs(last_page, url):
+    jobs = []
+    for page in range(last_page):
+    # for page in range(1):
+        print(f"Scraping Stackoverflow page {page + 1}")
+
+        try:
+            result = requests.get(f"{url}&pg={page + 1}")
+            if (result.status_code != 200):
+                print("error", result.status_code)
+                return []
+        except Exception as e:
+            print(e, f"{url}&pg={page + 1}")
+            return []
+
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.select("div.listResults > div.js-result")
+        
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    
+    return jobs
+
+def get_jobs(query):
+    url = f"https://stackoverflow.com/jobs?r=true&q={query}"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
+
+    return jobs
