ac0a16810c154233dce7bb069dbdaadecab28892
rosenrose <crazytempler@gmail.com>
2022-04-09 15:01:54 +0900

    flask

---
+++ exporter.py
@@ -0,0 +1,10 @@
+import csv
+
+def save_to_file(filename, jobs):
+  with open(filename, "w", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(jobs[0].keys())
+
+    for job in jobs:
+      writer.writerow(job.values())
+  return csv
--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from indeed import get_jobs as get_indeed_jobs
-from stackoverflow import get_jobs as get_stackoverflow_jobs
-from save import save_to_file
+from flask import Flask, render_template, request, redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
 
-indeed_jobs = get_indeed_jobs()
-stackoverflow_jobs = get_stackoverflow_jobs()
-jobs = indeed_jobs + stackoverflow_jobs
-save_to_file(jobs)
+app = Flask("SuperScrapper")
+cache = {}
+CSV_NAME = "jobs.csv"
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/report")
+def report():
+  if query := request.args.get("query"):
+    query = query.lower()
+    if query in cache:
+      jobs = cache[query]
+    else:
+      jobs = get_jobs(query)
+      cache[query] = jobs
+  else:
+    return redirect("/")
+
+  return render_template("report.html", resultsNumber=len(jobs), searchBy=query, jobs=jobs)
+
+@app.route("/export")
+def export():
+  try:
+    if query := request.args.get("query"):
+      query = query.lower()
+    else:
+      raise Exception()
+
+    jobs = cache[query]
+    save_to_file(CSV_NAME, jobs)
+    return send_file(CSV_NAME)
+  except:
+    return redirect("/")
+
+app.run(host="0.0.0.0")
