f9e8e152188e43fe97ca354b9f1f71ccdda6c093
rosenrose <crazytempler@gmail.com>
2022-09-25 19:49:29 +0900

    Cache timeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime
 from pathlib import Path
 from flask import Flask, render_template, request, redirect, send_file
 from extractors import wwr, indeed
@@ -8,7 +9,8 @@ from typing import Final
 
 USER_AGENT: Final = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
 
-cache: dict[str, list] = {}
+CACHE_REFRESH_RATE_SECONDS = 60 * 60
+cache: dict[str, dict] = {}
 if (saved_cache := Path("cache.json")).exists():
     cache = json.load(open(saved_cache, encoding="utf-8"))
 
@@ -28,16 +30,18 @@ def search():
         return redirect("/")
 
     query_key = query.lower()
+    is_refresh_force = request.args.get("refresh_force") != None
 
-    if query_key in cache:
-        jobs = cache[query_key]
-    else:
+    if is_ignore_cache(is_refresh_force, query_key):
         wwr_results = wwr.extract_jobs(query_key, USER_AGENT)
         indeed_results = indeed.extract_jobs(query_key, USER_AGENT)
 
         jobs = wwr_results + indeed_results
-        cache[query_key] = jobs
-        json.dump(cache, open(saved_cache, "w", encoding="utf-8"), ensure_ascii=False, indent=2)
+
+        cache[query_key] = {"timestamp": str(datetime.now()), "data": jobs}
+        json.dump(cache, open(saved_cache, "w", encoding="utf-8"))
+    else:
+        jobs = cache[query_key]["data"]
 
     return render_template("search.html", query=query, jobs=jobs)
 
@@ -55,7 +59,7 @@ def export():
     if (query_key not in cache) or (not format) or (not format.strip()):
         return redirect(f"/search?query={query}")
 
-    jobs = cache[query_key]
+    jobs = cache[query_key]["data"]
 
     match format.lower():
         case "csv":
@@ -68,6 +72,18 @@ def export():
     filename = f"{query}.{format.lower()}"
 
     return send_file(output, download_name=filename, as_attachment=True)
+
+
+def is_ignore_cache(is_refresh_force: bool, query_key: str) -> bool:
+    if is_refresh_force:
+        return True
+
+    if query_key not in cache:
+        return True
+
+    timestamp = datetime.fromisoformat(cache[query_key]["timestamp"])
+
+    return (datetime.now() - timestamp).seconds > CACHE_REFRESH_RATE_SECONDS
 
 
 if __name__ == "__main__":
